b2e6f5e6f6e50d9ec18631831a51ab902c59de5c
Niklas Fischer-Riepe <niklas@fischer-riepe.com>
2018-03-13 07:49:33 +0100

    Introduced more functionality.

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,7 @@
 
 namespace RebaseData;
 
+use ZipArchive;
 use GuzzleHttp\Client as GuzzleClient;
 use RebaseData\Exception\ConversionException;
 use RebaseData\Exception\InvalidArgumentException;
@@ -20,18 +21,32 @@ class Client
         ]);
     }
 
-    public function convertAndReceiveZip(array $inputFiles, $outputFile, array $options = [])
+    public function getCacheDir()
     {
-        $this->validateFiles($inputFiles);
-
-        if (strtolower(substr($outputFile, -4)) !== '.zip') {
-            throw new InvalidArgumentException('Output file must have .zip extension: '.$outputFile);
+        $cacheDir = sys_get_temp_dir().'/.rebasedata-php-client-cache';
+        if (!file_exists($cacheDir)) {
+            mkdir($cacheDir);
         }
 
-        if (empty($options['overwriteOutputFile']) and file_exists($outputFile)) {
-            throw new InvalidArgumentException('Output file already exists: '.$outputFile);
+        return $cacheDir;
+    }
+
+    public function getInputFilesHash(array $inputFiles)
+    {
+        $this->validateInputFiles($inputFiles);
+
+        $dataToHash = '';
+
+        foreach ($inputFiles as $inputFile) {
+            $dataToHash .= basename($inputFile).'='.sha1_file($inputFile)."\n";
         }
-        unset($options['overwriteOutputFile']);
+
+        return sha1($dataToHash);
+    }
+
+    public function convertAndReceiveZip(array $inputFiles, array $options = [])
+    {
+        $this->validateInputFiles($inputFiles);
 
         $parts = [];
         foreach ($inputFiles as $inputFile) {
@@ -47,27 +62,123 @@ class Client
             $queryString = '?'.http_build_query($options);
         }
 
+        $zipFile = $this->getCacheDir().'/'.$this->getInputFilesHash($inputFiles).'.zip';
+
+        if (file_exists($zipFile)) {
+            unlink($zipFile);
+        }
+
         $response = $this->guzzleClient->request('POST', 'convert'.$queryString, [
             'multipart' => $parts,
-            'sink' => $outputFile,
+            'sink' => $zipFile,
         ]);
 
         if ($response->getHeader('Content-Type')[0] === 'application/json') {
-            $json = json_decode(file_get_contents($outputFile), true);
-            unlink($outputFile);
+            $json = json_decode(file_get_contents($zipFile), true);
+            unlink($zipFile);
 
             throw new ConversionException($json['error']);
         }
 
-        return true;
+        return $zipFile;
     }
 
-    private function validateFiles(array $files)
+    public function convertAndReceiveCsvDirectory(array $inputFiles)
+    {
+        $this->validateInputFiles($inputFiles);
+
+        $csvDirectory = $this->getCacheDir().'/'.$this->getInputFilesHash($inputFiles).'-csv';
+        $csvDirectoryDoneMarker = $csvDirectory.'/done.marker';
+
+        if (file_exists($csvDirectory)) {
+            if (file_exists($csvDirectoryDoneMarker)) {
+                return $csvDirectory;
+            } else {
+                exec('rm -r '.escapeshellarg($csvDirectory));
+            }
+        }
+
+        mkdir($csvDirectory);
+
+        $zipFile = $this->convertAndReceiveZip($inputFiles);
+
+        $zipArchive = new ZipArchive();
+        $zipArchive->open($zipFile);
+        $zipArchive->extractTo($csvDirectory);
+
+        unlink($zipFile);
+
+        file_put_contents($csvDirectoryDoneMarker, '1');
+
+        return $csvDirectory;
+    }
+
+    public function getDatabaseTables(array $inputFiles)
+    {
+        $this->validateInputFiles($inputFiles);
+
+        $csvDirectory = $this->convertAndReceiveCsvDirectory($inputFiles);
+
+        $tables = [];
+        foreach ($this->getCsvFilesOfDirectory($csvDirectory) as $file) {
+            $tables[] = pathinfo($file, PATHINFO_FILENAME);
+        }
+
+        return $tables;
+    }
+
+    public function getDatabaseTableRows(array $inputFiles, $table)
+    {
+        $this->validateInputFiles($inputFiles);
+
+        $csvDirectory = $this->convertAndReceiveCsvDirectory($inputFiles);
+
+        $tableFile = $csvDirectory.'/'.$table.'.csv';
+
+        if (!file_exists($tableFile)) {
+            throw new InvalidArgumentException('Table does not exist: '.$table);
+        }
+
+        $h = fopen($tableFile, 'r');
+        if ($h === false) {
+            throw new InvalidArgumentException('Could not open table file: '.$tableFile);
+        }
+
+        $rows = [];
+        while (($row = fgetcsv($h, 0, ',')) !== false) {
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+
+    private function validateInputFiles(array $files)
     {
         foreach ($files as $file) {
             if (!file_exists($file)) {
                 throw new InvalidArgumentException('Input file does not exist: '.$file);
             }
         }
+    }
+
+    private function getCsvFilesOfDirectory($dir = '.')
+    {
+        $files = [];
+
+        $dh = opendir($dir);
+        if ($dh === false) {
+            throw new InvalidArgumentException('Could not open directory');
+        }
+
+        while (($file = readdir($dh)) !== false) {
+            if ($file == '.' or $file == '..' or substr($file, -4) !== '.csv') {
+                continue;
+            }
+
+            $files[] = $file;
+        }
+        closedir($dh);
+
+        return $files;
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -9,17 +9,16 @@ use RebaseData\Client;
 use RebaseData\Exception\ConversionException;
 
 $inputFiles = [dirname(__DIR__).'/samples/access.accdb'];
-$outputFile = '/tmp/out.zip';
-$options = ['overwriteOutputFile' => true, 'outputFormat' => 'xlsx'];
+$options = ['outputFormat' => 'xlsx'];
 
 try {
     echo "Executing conversion, this might take some time..\n";
 
-    $client = new Client('apiKey');
-    $client->convertAndReceiveZip($inputFiles, $outputFile, $options);
+    $client = new Client('freemium');
+    $zipFile = $client->convertAndReceiveZip($inputFiles, $options);
 
     echo "Conversion successful!\n";
-    echo "You can find the ZIP archive containing the XLSX files in $outputFile\n";
+    echo "You can find the ZIP archive containing the XLSX files in $zipFile\n";
 } catch (ConversionException $e) {
     echo "Conversion failed: ".$e->getMessage()."\n";
 }
